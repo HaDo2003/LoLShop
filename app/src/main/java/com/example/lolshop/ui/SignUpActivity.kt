bbb50126bd1f756a8dc1cba66a92f49c42ef9880
Thanh Nhat Tran <ginthanhnhat@gmail.com>
2024-12-07 01:02:12 +0700

    update Sign Up use Jetpack

--- app/src/main/java/com/example/lolshop/ui/SignUpActivity.kt
+++ app/src/main/java/com/example/lolshop/ui/SignUpActivity.kt
@@ -2,73 +2,163 @@ package com.example.lolshop.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lolshop.R
+import com.example.lolshop.ui.theme.LoLShopTheme
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.userProfileChangeRequest
+import com.google.firebase.auth.UserProfileChangeRequest
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : ComponentActivity() {
 
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup) // Set the signup screen layout
+        setContent {
+            SignUpScreen()
+        }
+    }
 
-        // Initialize Firebase Auth
+    @Composable
+    fun SignUpScreen() {
+        // FirebaseAuth instance
         auth = FirebaseAuth.getInstance()
 
-        val nameEditText = findViewById<EditText>(R.id.nameEditText)
-        val emailEditText = findViewById<EditText>(R.id.emailEditText)
-        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
-        val signUpButton = findViewById<Button>(R.id.signUpButton)
-        val alreadyHaveAccountTextView = findViewById<TextView>(R.id.alreadyHaveAccountTextView)
+        var name by remember { mutableStateOf("") }
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        var isLoading by remember { mutableStateOf(false) }
 
-        // Handle sign-up button click
-        signUpButton.setOnClickListener {
-            val name = nameEditText.text.toString().trim()
-            val email = emailEditText.text.toString().trim()
-            val password = passwordEditText.text.toString().trim()
-
+        fun onSignUp() {
             if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
-                Toast.makeText(this, "Please enter all fields", Toast.LENGTH_SHORT).show()
-            } else {
-                // Perform sign-up with Firebase
-                auth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful) {
-                            // Sign-up successful, you can update user's name if needed
-                            val user = auth.currentUser
-                            user?.let {
-                                val profileUpdates = userProfileChangeRequest {
-                                    displayName = name
-                                }
-                                user.updateProfile(profileUpdates)
-                                    .addOnCompleteListener { profileUpdateTask ->
-                                        if (profileUpdateTask.isSuccessful) {
-                                            // Navigate to login screen or main activity
-                                            Toast.makeText(this, "Sign-up successful", Toast.LENGTH_SHORT).show()
-                                            finish() // Close the SignUp screen
-                                        }
-                                    }
-                            }
-                        } else {
-                            Toast.makeText(this, "Sign-up failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
-                        }
-                    }
+                Toast.makeText(this@SignUpActivity, "Please enter all fields", Toast.LENGTH_SHORT).show()
+                return
             }
+
+            isLoading = true
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this@SignUpActivity) { task ->
+                    isLoading = false
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        user?.let {
+                            val profileUpdates = UserProfileChangeRequest.Builder()
+                                .setDisplayName(name)
+                                .build()
+                            user.updateProfile(profileUpdates)
+                                .addOnCompleteListener { profileUpdateTask ->
+                                    if (profileUpdateTask.isSuccessful) {
+                                        Toast.makeText(this@SignUpActivity, "Sign-up successful", Toast.LENGTH_SHORT).show()
+                                        finish() // Close the SignUp screen
+                                    }
+                                }
+                        }
+                    } else {
+                        Toast.makeText(this@SignUpActivity, "Sign-up failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
 
-        // Handle "Already have an account?" click
-        alreadyHaveAccountTextView.setOnClickListener {
-            // Navigate to the LoginActivity
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish() // Close the SignUp screen
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            // Logo
+            Image(
+                painter = painterResource(id = R.drawable.mobilelogo),
+                contentDescription = stringResource(id = R.string.logo),
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(Color.Black)
+            )
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            OutlinedTextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Name") },
+                modifier = Modifier.fillMaxWidth(),
+                singleLine = true
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = { /* Handle next action */ }
+                ),
+                singleLine = true
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = { /* Handle done action */ }
+                ),
+                visualTransformation = PasswordVisualTransformation(),
+                singleLine = true
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = { onSignUp() },
+                modifier = Modifier.fillMaxWidth(),
+                enabled = !isLoading
+            ) {
+                Text(text = if (isLoading) "Signing Up..." else "Sign Up")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextButton(onClick = {
+                // Handle Already have account action
+                val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
+                startActivity(intent)
+                finish() // Close the SignUp screen
+            }) {
+                Text(text = "Already have an account? Sign In", color = MaterialTheme.colorScheme.primary)
+            }
         }
     }
 }
+
