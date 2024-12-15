ebcb66c9728ab2554ef52d4df31b8c1fd7fc7097
Ha Do <118713300+HaDo2003@users.noreply.github.com>
2024-12-15 14:18:21 +0700

    Update model

--- app/src/main/java/com/example/lolshop/model/Product.kt
+++ app/src/main/java/com/example/lolshop/model/Product.kt
@@ -1,10 +1,13 @@
 package com.example.lolshop.model
 
+import java.io.Serializable
+
 data class Product(
     val id: String = "",
     val name: String = "",
     val price: String = "",
     val description: String = "",
     val categoryId: String = "",
+    val showRecommended: Boolean = false,
     val imageUrl: String = ""
-)
+) : Serializable
