e205c0748616ee64ca38feb0db08a08b19d48cf0
Trần Thanh Huy <94445823+bazabolo@users.noreply.github.com>
2024-12-25 01:08:00 +0700

    fix detail activity

--- app/src/main/java/com/example/lolshop/view/homepage/DetailActivity.kt
+++ app/src/main/java/com/example/lolshop/view/homepage/DetailActivity.kt
@@ -34,23 +34,29 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
-import coil.compose.rememberAsyncImagePainter
+
 import com.example.lolshop.R
 
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
-import androidx.compose.ui.text.style.TextAlign
+
 import androidx.compose.ui.unit.sp
 import com.example.lolshop.model.Product
 import com.example.lolshop.view.BaseActivity
+
+import androidx.compose.ui.draw.clip
+
+import coil.compose.AsyncImage
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.getValue
+
+import androidx.core.content.ContextCompat.startActivity
 
 
 class DetailActivity : BaseActivity() {
@@ -85,9 +91,6 @@ class DetailActivity : BaseActivity() {
         onAddToCartClick: () -> Unit,
         onCartClick: () -> Unit,
     ) {
-        var selectedImageUrl by remember { mutableStateOf(product.imageUrl.first()) }
-        var selectedModelIndex by remember { mutableStateOf(-1) }
-
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -96,94 +99,119 @@ class DetailActivity : BaseActivity() {
         ) {
             ConstraintLayout(
                 modifier = Modifier
-                    .fillMaxSize()
+                    .fillMaxWidth()
                     .height(430.dp)
                     .padding(bottom = 16.dp)
             ) {
-                val (back,mainImage,)=createRefs()
-                Image(
-                    painter = rememberAsyncImagePainter(model = selectedImageUrl),
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop,
+                val (back, mainImage) = createRefs()
+
+                Box(
                     modifier = Modifier
-                        .fillMaxSize()
-                        .constrainAs(mainImage){
+                        .fillMaxWidth()
+                        .height(400.dp)
+                        .constrainAs(mainImage) {
                             top.linkTo(parent.top)
-                            bottom.linkTo(parent.bottom)
-                            end.linkTo(parent.end)
                             start.linkTo(parent.start)
+                            end.linkTo(parent.end)
                         }
-                )
+                ) {
+                    if (product.imageUrl.isNotEmpty()) {
+                        AsyncImage(
+                            model = product.imageUrl,
+                            contentDescription = product.name,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .clip(RoundedCornerShape(10.dp)), // Rounded corners
+                            contentScale = ContentScale.Crop
+                        )
+                    } else {
+                        Text(
+                            text = "No Image Available",
+                            style = androidx.compose.material.MaterialTheme.typography.body2,
+                            color = Color.Gray,
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
+                }
+
                 Image(
                     painter = painterResource(R.drawable.back),
-                    contentDescription = "",
+                    contentDescription = "Back Button",
                     modifier = Modifier
-                        .padding(top=48.dp)
-                        .clickable{onBackClick()}
-                        .constrainAs(back){
+                        .padding(top = 48.dp)
+                        .clickable { onBackClick() }
+                        .constrainAs(back) {
                             top.linkTo(parent.top)
                             start.linkTo(parent.start)
                         }
                 )
-
-
             }
-            Row(verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(top=16.dp)
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(top = 16.dp)
                     .padding(horizontal = 16.dp)
-            ){
+            ) {
                 Text(
-                    text=product.name,
+                    text = product.name,
                     fontSize = 23.sp,
                     modifier = Modifier
                         .fillMaxWidth()
                         .weight(1f)
-                        .padding(end=16.dp)
+                        .padding(end = 16.dp)
                 )
                 Text(
-                    text="$${product.price}",
+                    text = "$${product.price}",
                     fontSize = 22.sp
                 )
-                Text(text=product.description,
-                    fontSize = 14.sp,
-                    color = Color.Black,
-                    modifier = Modifier.padding(16.dp)
-                )
-                Row (
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.fillMaxWidth().padding(18.dp)
-                ){
-                    IconButton(
-                        onClick = onCartClick,
-                        modifier = Modifier.background(
-                            colorResource(R.color.purple_700),
-                            shape = RoundedCornerShape(10.dp)
-                        )
-                    ) {
-                        Icon(
-                            painter = painterResource(id= R.drawable.btn_2),
-                            contentDescription =  "Cart",
-                            tint= Color.Black
-                        )
-                    }
-                    Button(
-                        onClick = onAddToCartClick,
-                        shape = RoundedCornerShape(10.dp),
-                        colors = ButtonDefaults.buttonColors(
-                            contentColor = colorResource(R.color.purple_700)
-                        ),
-                        modifier = Modifier
-                            .weight(1f)
-                            .padding(start = 8.dp)
-                            .height(50.dp)
-                    ) {
-                        Text(
-                            text = " Add to cart", fontSize = 18.sp
-                        )
-                    }
+            }
+
+            Text(
+                text = product.description,
+                fontSize = 14.sp,
+                color = Color.Black,
+                modifier = Modifier.padding(16.dp)
+            )
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(18.dp)
+            ) {
+                IconButton(
+                    onClick = onCartClick,
+                    modifier = Modifier.background(
+                        colorResource(R.color.purple_700),
+                        shape = RoundedCornerShape(10.dp)
+                    )
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.btn_2),
+                        contentDescription = "Cart",
+                        tint = Color.Black
+                    )
+                }
+                Button(
+                    onClick = onAddToCartClick,
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = colorResource(R.color.purple_700)
+                    ),
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(start = 8.dp)
+                        .height(50.dp)
+                ) {
+                    Text(
+                        text = " Add to cart", fontSize = 18.sp
+                    )
                 }
             }
         }
     }
+
+
 
 }
